47909709ea61ed904486ad7a068fe7f4abfdadcc
Ramos <Edan.Ramos@coxautoinc.com>
2020-07-27 16:33:57 -0600

    Built our first class component

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = (title, content) => {
+  return (
+  <div className="input"> 
+    <input />
+  <label>{title}</label>
+  </div>)
+}
+
+export default Input;
