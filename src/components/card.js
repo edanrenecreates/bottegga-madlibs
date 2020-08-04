76392f48f7bef8f2865fe479c0ef6ab0f8fe8369
Ramos <Edan.Ramos@coxautoinc.com>
2020-08-04 11:17:08 -0600

    copied files from bottega site 8/4/2020

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-
 import Input from './input';
-
 class Card extends Component {
-  render () {
-    return (
-      <div className="card">
-          { Input('Color') }
-          { Input('Plural Noun') }
-      </div>
-    )
-  }
-}
+    constructor() {
+        super()
+        this.state = {
+            color: 'BLUE',
+            pluralNoun: ''
+        }
+    }
 
+    handleInputChange() {
+        this.setState({ color: 'red' })
+        this.handleInputChange = this.handleInputChange.bind(this);
+    } 
+
+    handleInputChange(event) {
+        this.setState({ color: event.target.value })
+    }
+
+    render() {
+
+        return (
+            <div className="card">
+                <h1>{this.state.color}</h1>
+                <input/>
+                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
+            </div>
+        )
+
+        return (
+            <div className="card">
+                <h1>{this.state.color}</h1>
+                { Input('Color') }
+                { Input('Plural Noun') }
+            </div>
+        )
+    }
+}
 export default Card;
