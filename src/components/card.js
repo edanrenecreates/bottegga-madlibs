03285467150c82e3115cfbf5f30b582c67f209cd
Ramos <Edan.Ramos@coxautoinc.com>
2020-08-11 14:55:40 -0600

    finished clear form functioinallity

--- src/components/card.js
+++ src/components/card.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
+
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+}
 
 class Card extends Component {
 
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: ''
-
-        }
+        this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+
     } 
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
-        console.log(this.state);
+        
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({  contentVisible: true})
+        }        
     }
 
     render() {
@@ -60,11 +75,18 @@ class Card extends Component {
         ]
 
         return (
-            <div className="card">
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
                 {
                     inputData.map(data => Input( (data), this.handleInputChange ))
+                }                
+                </div>
+            <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Mad Lib'}</button>
+                {
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
-            </div>
+                
+            </form>
         )
     }
 }
