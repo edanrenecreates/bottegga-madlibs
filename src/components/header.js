18a0ea424136dad7f10822bb230fd8287943dceb
Ramos <Edan.Ramos@coxautoinc.com>
2020-08-13 15:05:15 -0600

    styled the generate button

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,19 @@
-import React from 'react';
 
-const Header = (title, content) => {
+import React from "react";
+
+const Header = () => {
   return (
-  <div className="header"> 
-    <h1>Bottega Madlibs</h1>
-    <p>Fill out the fields below and click the Generate button to see the Mad Lib story.</p>
-  </div>)
-}
+    <div className="header">
+      <div className="header__skew"></div>
+      <div className="header__border"></div>
+      <h1> Bottega Madlibs </h1>{" "}
+      <p>
+        {" "}
+        Fill out the fields below and click the generate button to see the Mad
+        Lib story.{" "}
+      </p>{" "}
+    </div>
+  );
+};
 
 export default Header;
--- src/components/input.js
+++ src/components/input.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 const Input = ({title, state, name}, onChange, index) => {
